7a15f5080ce61cf6efb7a7aac4c81d658e07df75
Fatali <fataliamirli331@gmail.com>
2023-06-19 00:23:39 -0700

    js/app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,40 @@
-function bot(){
-    let inp_value = document.getElementById('group').value;
-    if(inp_value.slice(-3,-2)==1){
-        document.body.style.backgroundColor = 'pink'
-        document.getElementById('grnm').innerHTML = 'Morning Group'
-        document.getElementById('grnm').style.color = 'red'
-    } else if(inp_value.slice(-3,-2)==1){
-        document.body.style.backgroundColor = 'red'
-        document.getElementById('grnm').innerHTML = 'Afternoon Group'
-        document.getElementById('grnm').style.color = 'yellow'
-    } else if(inp_value.slice(-3,-2)==1){
-        document.body.style.backgroundColor = 'blue'
-        document.getElementById('grnm').innerHTML = 'Evening Group'
-        document.getElementById('grnm').style.color = 'black'
-    }
-}
+let btn = document.querySelector('.btn');
+let alertbox = document.querySelector('.alertbox');
 
+btn.addEventListener('click', function(){
+    let task = document.querySelector('.task-box input').value;
 
-function snmn(){
-    if(document.body.style.backgroundColor == 'bisque'){
-        document.body.style.backgroundColor = 'gray'
-        document.getElementById('gun').style.backgroundColor = 'bisque'
-        document.getElementById('gun').style.color = 'gray'
-        document.getElementById('ay').style.backgroundColor = 'bisque'
-        document.getElementById('ay').style.color = 'gray'
+    let h2 = document.createElement('h2');
+    let div = document.createElement('div');
+    let button = document.createElement('button');
+
+    h2.innerHTML= task;
+    button.innerHTML = 'Delete';
+    div.setAttribute('class', 'box');
+    button.setAttribute('class', 'btn btn-danger delete_btn')
+
+    div.appendChild(h2)
+    div.appendChild(button)
+    document.querySelector('.task-list').appendChild(div)
+    
+    alertbox.style.right = '2%'
+    alertbox.classList.add('bg-success')
+    alertbox.innerHTML = 'Task added'
+    setTimeout(() => {
+        alertbox.style.right = '-20%' 
+    }, 1000);
+
+   let all_btns = document.querySelectorAll('.delete_btn');
+
+   for(let btn of all_btns){
+    btn.onclick = function(){
+        this.parentElement.remove();
+        alertbox.style.right = '2%'
+        alertbox.classList.add('bg-danger')
+        alertbox.innerHTML = 'Task deleted'
+        setTimeout(() => {
+            alertbox.style.right = '-20%' 
+        }, 1000);
     }
-}
+   }
+})  
